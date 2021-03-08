7576af80dba3defec04c9293cad50cc099f8f258
Leonardo Ramljak <leonardo.ramljak@gmail.com>
2021-03-08 18:21:47 +0100

    Moving the file pointer

---
+++ php_sandbox/file_details.php
@@ -0,0 +1,30 @@
+<?php
+
+$filepath = __DIR__ . '/sonnet.txt';
+
+echo is_readable($filepath) ? 'readable' : 'not';
+echo "<br />";
+echo is_writable($filepath) ? 'writable' : 'not';
+echo "<br />";
+echo is_executable($filepath) ? 'executable' : 'not';
+echo "<br />";
+
+echo "<br />";
+echo filemtime($filepath) . "<br />";
+echo strftime('%m/%d/%Y %H:%M', filemtime($filepath))  . "<br />";
+echo strftime('%m/%d/%Y %H:%M', filectime($filepath))  . "<br />";
+echo strftime('%m/%d/%Y %H:%M', fileatime($filepath))  . "<br />";
+
+echo "<br />";
+
+$path = pathinfo($filepath);
+
+echo $path['dirname'] . '<br />';
+echo $path['basename'] . '<br />';
+echo $path['filename'] . '<br />';
+echo $path['extension'] . '<br />';
+
+echo '<br />';
+
+echo dirname($filepath) . '<br />';
+echo basename($filepath) . '<br />';
