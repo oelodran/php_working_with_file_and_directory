16e9c7d98cacf93c2d23877938ee15c7461d64ae
Leonardo Ramljak <leonardo.ramljak@gmail.com>
2021-03-09 21:53:16 +0100

    Upload file

--- php_sandbox/upload_form.php
+++ php_sandbox/upload_form.php
@@ -1,2 +1,16 @@
-<?php
-
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>File Upload</title>
+</head>
+<body>
+<form action="upload.php" method="post" enctype="multipart/form-data">
+    <input type="file" name="file_upload" /><br><br>
+    <input type="submit" name="submit" value="Upload" />
+</form>
+</body>
+</html>
